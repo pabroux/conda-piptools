b0af6a23976fdc14149ab40da303ec4c049e616d
pabroux <pierre-alexandrebroux@live.fr>
2021-05-18 09:55:59 +0200

    [UPDATE] - Changing setEnvName.sh.

--- setEnvName.sh
+++ setEnvName.sh
@@ -3,9 +3,9 @@
 # Set the name for the new conda environment
 if [ $# -eq 1 ]
 then
-		str=$(echo "$1" | tr -d '[:punct:]' | sed 's/\ /_/g')
-        sed 's/.*CONDA_ENV.*/CONDA_ENV='"$str"'/' Makefile > Makefile
-		sed 's/.*name:.*/name: '"$str"'/' environment.yml > environment.yml
+		str=$(echo "$1" | sed 's/\ /_/g')
+		echo "$(sed 's/.*CONDA_ENV=.*/CONDA_ENV='"$str"'/' Makefile)" > Makefile
+		echo "$(sed 's/.*name: .*/name: '"$str"'/' environment.yml)" > environment.yml
 		echo 'setEnvName.sh: environment name successfully changed.'
 else
         echo 'setEnvName.sh: invalid argument number.'

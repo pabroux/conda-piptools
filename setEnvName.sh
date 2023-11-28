8ee4ff483cde20c6bc6d704eae439d2972b1440d
pabroux <pierre-alexandrebroux@live.fr>
2023-11-28 17:41:57 +0000

    [U] setEnvName.sh.

--- setEnvName.sh
+++ setEnvName.sh
@@ -4,8 +4,8 @@
 if [ $# -eq 1 ]
 then
 		str=$(echo "$1" | sed 's/\ /_/g')
-		echo "$(sed 's/.*CONDA_ENV=.*/CONDA_ENV='"$str"'/' Makefile)" > Makefile
-		echo "$(sed 's/.*name: .*/name: '"$str"'/' environment.yml)" > environment.yml
+		echo "$(sed 's/CONDA_ENV=.*/CONDA_ENV='"$str"'/' Makefile)" > Makefile
+		echo "$(sed 's/name: .*/name: '"$str"'/' environment.yml)" > environment.yml
 		echo 'setEnvName.sh: environment name successfully changed.'
 else
         echo 'setEnvName.sh: invalid argument number.'

9641d27dfabfcd1f772d2ce3c0ddc20a4dbabc54
pabroux <pierre-alexandrebroux@live.fr>
2025-03-12 21:03:21 +0100

    style: format shell scripts with shfmt.

--- set_env_name.sh
+++ set_env_name.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Set the name for the new conda environment
-if [ $# -eq 1 ]
-then
-		str=$(echo "$1" | sed 's/\ /_/g')
-		echo "$(sed 's/CONDA_ENV=.*/CONDA_ENV='"$str"'/' Makefile)" > Makefile
-		echo "$(sed 's/name: .*/name: '"$str"'/' environment.yml)" > environment.yml
-		echo 'setEnvName.sh: environment name successfully changed.'
+if [ $# -eq 1 ]; then
+	str=$(echo "$1" | sed 's/\ /_/g')
+	echo "$(sed 's/CONDA_ENV=.*/CONDA_ENV='"$str"'/' Makefile)" >Makefile
+	echo "$(sed 's/name: .*/name: '"$str"'/' environment.yml)" >environment.yml
+	echo 'setEnvName.sh: environment name successfully changed.'
 else
-        echo 'setEnvName.sh: invalid argument number.'
-		echo 'Syntax: ./setEnvName.sh "name"'
+	echo 'setEnvName.sh: invalid argument number.'
+	echo 'Syntax: ./setEnvName.sh "name"'
 fi
